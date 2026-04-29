4c5d1eb6458f8ca3f88af792b747170cad110664
luis <senaluissilva883@gmail.com>
2026-04-29 12:25:05 -0500

    learn

--- fundamentos.py
+++ fundamentos.py
@@ -1,2 +1,67 @@
 NOMBRE = "luis"
 print(" mi nombre es " + NOMBRE)
+
+#tipo de escritura de variables
+
+
+snake_case = "sena"
+print(snake_case)
+
+#Cuidaddo con las mayusculas y minusculas en las variables, ya que python es case sensitive
+X = "esta una X en mayuscula"
+print(X)
+
+#TIPOS DE DATOS EN PYTHON
+nombre = "luis"
+apellido = "SILVA"
+edad = 21
+altura = 1.80
+activo = True
+correo = "senaluissilva883@gmail.com"
+telefono = "3502222057"
+cedula = 1234567890
+
+# conversion de tipos de datos
+telefono_int = int(telefono) #3502222057
+edad_float = float(edad) #21.0
+altura_int = int(altura) #1
+cedula_str = str(cedula) #"1234567890"
+
+#impresion de tipos de datos
+print (type(nombre), nombre)
+print (type(apellido), apellido)
+print (type(edad), edad)
+print (type(altura), altura)
+print (type(activo), activo)
+print (type(correo), correo)
+print (type(telefono), telefono)
+print (type(cedula), cedula)
+
+# conversion de tipos de datos
+print (type(telefono_int), telefono_int)
+print (type(edad_float), edad_float)
+print (type(altura_int), altura_int)
+print (type(cedula_str), cedula_str)
+
+"""Comentarios de bloque"""
+
+#identacion en python
+if 5 > 2:
+    print ("5 es mayor que 2")
+else :
+    print ("5 no es mayor que 2")
+
+
+
+#inputs en python
+nombre_usuario = input("Ingrese su nombre: ")
+print("Hola " + nombre_usuario + ", bienvenido a python")
+
+edad_usuario = int(input("Ingrese su edad: "))
+print("Tu edad es :", edad_usuario, "años")
+
+# imprimir con formato f-string "f" es para combinar
+print(f"hola {nombre_usuario}, tienes {edad_usuario} años")
+
+
+
---
+++ operadores.py
@@ -0,0 +1,31 @@
+#operadores aritméticos
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+tipo_operacion = input("Ingrese el tipo de operación (suma, resta, multiplicacion, division, division_entera, modulo, potencia): ")
+
+if tipo_operacion == "suma":
+    resultado = a + b
+elif tipo_operacion == "resta":
+    resultado = a - b
+elif tipo_operacion == "multiplicacion":
+    resultado = a * b
+elif tipo_operacion == "division":
+    resultado = a / b
+elif tipo_operacion == "division_entera":
+    resultado = a // b
+elif tipo_operacion == "modulo":
+    resultado = a % b
+elif tipo_operacion == "potencia":
+    resultado = a ** b
+else:
+    print("Operación no reconocida")
+    exit()
+
+
+print(f"Resultado: {resultado}")
+
+
+
+
+
+
